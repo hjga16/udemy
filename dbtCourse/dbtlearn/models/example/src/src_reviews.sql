82f7716846e79a228b687a8d5baad44db13e31e0
hjga16 <hjga16@gmail.com>
2022-09-22 18:02:18 -0500

    Models

---
+++ dbtCourse/dbtlearn/models/example/src/src_reviews.sql
@@ -0,0 +1,16 @@
+WITH raw_reviews
+AS
+(
+    SELECT * FROM AIRBNB.RAW.RAW_REVIEWS
+    
+)
+
+SELECT
+    LISTING_ID
+    , DATE AS review_date
+    , REVIEWER_NAME 
+    , COMMENTS AS review_text
+    , SENTIMENT as review_sentiment
+
+FROM
+    raw_reviews
