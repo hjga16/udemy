fb3f25ab84f530fd0b0d294a967106860cf05f36
hjga16 <hjga16@gmail.com>
2022-10-18 10:52:25 -0500

    18-10-2022 changes

--- dbtCourse/dbtlearn/models/fct/fct_reviews.sql
+++ dbtCourse/dbtlearn/models/fct/fct_reviews.sql
@@ -1,21 +1,17 @@
 {{
-        config(
-            materialized = 'incremental',
-            on_schema_change = 'fail'
-        )
-
-    }}
-
-
+  config(
+    materialized = 'incremental',
+    on_schema_change='fail'
+    )
+}}
 WITH src_reviews AS (
   SELECT * FROM {{ ref('src_reviews') }}
 )
 SELECT 
-    *
-FROM    
-    src_reviews
-WHERE 
-    review_text is not null
+  {{ dbt_utils.surrogate_key(['listing_id', 'review_date', 'reviewer_name', 'review_text']) }} as review_id,
+  *
+FROM src_reviews
+WHERE review_text is not null
 {% if is_incremental() %}
-    AND review_date > (SELECT MAX(review_date) FROM {{ this }})
+  AND review_date > (select max(review_date) from {{ this }})
 {% endif %}
